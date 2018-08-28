ecfdab263d7316690d4cd38706acfffd3a193610
indirajhenny <indirajhenny@knights.ucf.edu>
2018-08-28 14:11:04 -0700

    Add existing files

---
+++ pynqcopter/pidFloat/pidFloat/pidFloat.cache/ip/2017.4/f0572283c50a34db/pidFloat_pidfloat_0_0_stub.vhdl
@@ -0,0 +1,49 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+-- Date        : Tue Aug 28 13:29:08 2018
+-- Host        : fabricant running 64-bit Linux Mint 18 Sarah
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ pidFloat_pidfloat_0_0_stub.vhdl
+-- Design      : pidFloat_pidfloat_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    s_axi_CTRL_AWADDR : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    s_axi_CTRL_AWVALID : in STD_LOGIC;
+    s_axi_CTRL_AWREADY : out STD_LOGIC;
+    s_axi_CTRL_WDATA : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_CTRL_WSTRB : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s_axi_CTRL_WVALID : in STD_LOGIC;
+    s_axi_CTRL_WREADY : out STD_LOGIC;
+    s_axi_CTRL_BRESP : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_CTRL_BVALID : out STD_LOGIC;
+    s_axi_CTRL_BREADY : in STD_LOGIC;
+    s_axi_CTRL_ARADDR : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    s_axi_CTRL_ARVALID : in STD_LOGIC;
+    s_axi_CTRL_ARREADY : out STD_LOGIC;
+    s_axi_CTRL_RDATA : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_CTRL_RRESP : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_CTRL_RVALID : out STD_LOGIC;
+    s_axi_CTRL_RREADY : in STD_LOGIC;
+    ap_clk : in STD_LOGIC;
+    ap_rst_n : in STD_LOGIC;
+    interrupt : out STD_LOGIC
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "s_axi_CTRL_AWADDR[7:0],s_axi_CTRL_AWVALID,s_axi_CTRL_AWREADY,s_axi_CTRL_WDATA[31:0],s_axi_CTRL_WSTRB[3:0],s_axi_CTRL_WVALID,s_axi_CTRL_WREADY,s_axi_CTRL_BRESP[1:0],s_axi_CTRL_BVALID,s_axi_CTRL_BREADY,s_axi_CTRL_ARADDR[7:0],s_axi_CTRL_ARVALID,s_axi_CTRL_ARREADY,s_axi_CTRL_RDATA[31:0],s_axi_CTRL_RRESP[1:0],s_axi_CTRL_RVALID,s_axi_CTRL_RREADY,ap_clk,ap_rst_n,interrupt";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "pidfloat,Vivado 2017.4";
+begin
+end;
