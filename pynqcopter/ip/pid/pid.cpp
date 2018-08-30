a1b0e286e64a66d6c70c010942fb7d55562a8645
Brennan Cain <brenn.cain@gmail.com>
2018-08-30 14:06:36 -0700

    Stable flight

--- pynqcopter/ip/pid/pid.cpp
+++ pynqcopter/ip/pid/pid.cpp
@@ -51,9 +51,9 @@
 */
 void pid (F16_t rcCmdIn[5],
 		F16_t measured[4],
-		F32_t kp[2],
-		F32_t kd[2],
-		F32_t ki[2],
+		F32_t kp[3],
+		F32_t kd[3],
+		F32_t ki[3],
 		F16_t commandOut[4096]) {
 
 	//SETUP PRAGMAS
@@ -67,43 +67,40 @@ void pid (F16_t rcCmdIn[5],
 
 	#pragma HLS PIPELINE
 
-
-
-
-	static F16_t last_cmd_in[2]={0,0};
-	static F16_t last_measured[2]={0,0};
-	static F16_t last_error[2]={0,0};
-	static F32_t integral[2]={0,0};
-	static F32_t acc[2]={0,0};
-	F32_t pid_o[2];
-	F32_t curr_error[2];
-	F32_t deriv[2];
-	F32_t correction[2];
+	static F16_t last_error[3]={0,0,0};
+	static F32_t integral[3]={0,0,0};
+	F32_t pid_o[3];
+	F32_t curr_error[3];
+	F32_t deriv[3];
+	F32_t correction[3];
 	/****************************************
 						ROLL PID CONTROLLER
 	*****************************************/
 
-	curr_error[0]= rcCmdIn[0] - measured[0];
-	integral[0] =  clip(F32_t(integral[0] + (curr_error[0])),F32_t(-100),F32_t(100));
+	curr_error[0]= rcCmdIn[0] - measured[0]*5;
+	integral[0] =  clip(F32_t(integral[0] + curr_error[0]),F32_t(-100),F32_t(100));
 	deriv[0] = (curr_error[0]-last_error[0]);
 	correction[0] = (kp[0] * curr_error[0]) + (ki[0] * integral[0]) + (kd[0] * deriv[0]);
 	pid_o[0] = clip(correction[0],F32_t(-1),F32_t(.999));
 	last_error[0] = curr_error[0];
-	last_cmd_in[0]=rcCmdIn[0];
-	last_measured[0]=measured[0];
 
 	/****************************************
 						pitch PID CONTROLLER
 	*****************************************/
 
-	curr_error[1] = rcCmdIn[1] + measured[1];//sensor mounted facing forward but upside down
-	integral[1] =  clip(F32_t(integral[1] + (curr_error[1]*acc[1])),F32_t(-100),F32_t(100));
-	deriv[1] = (curr_error[1]-last_error[1])/acc[1];
+	curr_error[1] = rcCmdIn[1] + measured[1]*5;
+	integral[1] =  clip(F32_t(integral[1] + curr_error[1]),F32_t(-100),F32_t(100));
+	deriv[1] = (curr_error[1]-last_error[1]);
 	correction[1] = (kp[1] * curr_error[1]) + (ki[1] * integral[1]) + (kd[1] * deriv[1]);
 	pid_o[1] = clip(correction[1],F32_t(-1),F32_t(.999));
 	last_error[1] = curr_error[1];
-	last_cmd_in[1]=rcCmdIn[1];
-	last_measured[1]=measured[1];
+
+	/****************************************
+						yaw P CONTROLLER
+	*****************************************/
+
+	pid_o[2] = kp[2]*(rcCmdIn[3] + measured[3]*10);
+
 
 	// mixed _in contains noramlized values for each channel
 	// lets convert those to what we want to use
@@ -111,7 +108,7 @@ void pid (F16_t rcCmdIn[5],
 	F19_t r_c = pid_o[0];
 	F19_t p_c = pid_o[1];
 	F19_t t_c = rcCmdIn[2]*F16_t(.5)+F16_t(.5);//move scale to [-.5,.5) then [0,1)
-	F19_t y_c = rcCmdIn[3];
+	F19_t y_c = pid_o[2];
 
 	for(char i=0; i < 6; i++) {
 	#pragma HLS unroll
